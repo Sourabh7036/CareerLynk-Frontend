af3378e0e9c3f948801886ef1990da3a5c6ec7f9
Sobu shah <sobukumar568@gmail.com>
2026-07-10 03:41:45 +0530

    fixed auth issue

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,4 @@
-export const BACKEND_URL = import.meta.env.VITE_API_URL;
-
+export const BACKEND_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 export const USER_API_END_POINT = `${BACKEND_URL}/api/v1/user`;
 export const COMPANY_API_END_POINT = `${BACKEND_URL}/api/v1/company`;
 export const JOB_API_END_POINT = `${BACKEND_URL}/api/v1/job`;
